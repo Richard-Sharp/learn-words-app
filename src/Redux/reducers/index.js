4ac742f1a5ab3105ba87cf5cf6ee61cd844b550b
Richard-Sharp <novitskii.a@gmail.com>
2021-01-21 22:48:46 +0300

    Create new Reducer: cardListReducer.

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counterReducer from "./counterReducer";
 import userReducer from "./userReducer";
+import cardListReducer from "./cardListReducer";
 
 export default combineReducers({
 	user: userReducer,
 	counter: counterReducer,
+	cardList: cardListReducer
 });
