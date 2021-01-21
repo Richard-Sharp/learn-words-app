4ed338648125e7921f738fafe7852e8e141f28cb
Richard-Sharp <novitskii.a@gmail.com>
2021-01-21 22:47:51 +0300

    Create new Reducer: cardListReducer.

--- src/Redux/actions/actionsTypes.js
+++ src/Redux/actions/actionsTypes.js
@@ -1,3 +1,11 @@
+//userReducer
 export const ADD_USER = 'USER/ADD_USER';
+
+//counterReducer
 export const PLUS = 'COUNTER/PLUS';
 export const MINUS = 'COUNTER/MINUS';
+
+//cardListReducer
+export const FETCH_CARD_LIST = 'CARD_LIST/FETCH_CARD_LIST';
+export const FETCH_CARD_LIST_RESOLVE = 'CARD_LIST/FETCH_CARD_LIST_RESOLVE';
+export const FETCH_CARD_LIST_REJECT = 'CARD_LIST/FETCH_CARD_LIST_REJECT';
---
+++ src/Redux/reducers/cardListReducer.js
@@ -0,0 +1,35 @@
+import {
+	FETCH_CARD_LIST,
+	FETCH_CARD_LIST_REJECT,
+	FETCH_CARD_LIST_RESOLVE} from "../actions/actionsTypes";
+
+const cardListReducer = (state, action) => {
+	switch (action.type) {
+		case FETCH_CARD_LIST:
+			return {
+				payload: [],
+				error: null,
+				isBusy: true
+			}
+		case FETCH_CARD_LIST_RESOLVE:
+			return {
+				payload: action.payload,
+				error: null,
+				isBusy: false
+			}
+		case FETCH_CARD_LIST_REJECT:
+			return {
+				payload: null,
+				error: action.err,
+				isBusy: false
+			}
+		default:
+			return {
+				payload: null,
+				error: null,
+				isBusy: false
+			}
+	}
+}
+
+export default cardListReducer;
